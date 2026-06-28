25c6fedd4e4cb46188c9942d5a51573bf544df0c
deme005 <d_sheklashvili@cu.edu.ge>
2026-06-28 20:34:35 +0400

    done

---
+++ lecture16/task1/Helper/Algorithms.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace task1
+{
+    internal class Algorithms
+    {
+        public static IEnumerable<T> Where<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> OrderBy<T, Tkey>(IEnumerable<T> collection, Func<T, Tkey> func)
+            where Tkey : IComparable<Tkey>
+        {
+            List<T> list = new List<T>(collection);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    Tkey keyA = func(list[i]);
+                    Tkey keyB = func(list[j]);
+
+                    if (keyA.CompareTo(keyB) > 0)
+                    {
+                        T temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+
+        public static T First<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }
+            throw new InvalidOperationException("No matching element found!");
+        }
+
+        public static T FirstOrDefault<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+        }
+
+        public static T Single<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+            int counter = 0;
+            T value = default;
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    counter++;
+                    value = item;
+
+                    if (counter > 1)
+                    {
+                        throw new InvalidOperationException("This condition has at least one duplicate");
+                    }
+                }
+            }
+            if (counter == 1)
+            {
+                return value;
+            }
+            throw new InvalidOperationException("No matching element was found.");
+
+        }
+
+        public static T SingleOrDefault<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+            int counter = 0;
+            T value = default;
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    counter++;
+                    value = item;
+
+                    if (counter > 1)
+                    {
+                        throw new InvalidOperationException("This conditions has at least one duplicate");
+                    }
+                }
+            }
+            if (counter == 1)
+            {
+                return value;
+            }
+            return default;
+
+        }
+
+        public static bool Any<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool All<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+
+            foreach (var item in collection)
+            {
+                if (!func(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int Count<T>(IEnumerable<T> collection, Func<T, bool> func)
+        {
+            int counter = 0;
+            foreach (var item in collection)
+            {
+                if (func(item))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public static IEnumerable<T> Distinct<T>(IEnumerable<T> collection)
+        {
+            HashSet<T> seenItems = new HashSet<T>();
+
+            foreach (var item in collection)
+            {
+                if (seenItems.Add(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ lecture16/task1/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 5, 12, 3, 12, 8, 20 };
+            Dictionary<string, int> dict = new Dictionary<string, int>
+            {
+                { "A", 10 },
+                { "B", 50 },
+                { "C", 5 }
+            };
+
+            // 1. Where
+            var resWhereList = Algorithms.Where(list, x => x > 10);
+            var resWhereDict = Algorithms.Where(dict, x => x.Value > 10);
+
+            // 2. OrderBy
+            var resOrderList = Algorithms.OrderBy(list, x => x);
+            var resOrderDict = Algorithms.OrderBy(dict, x => x.Value);
+
+            // 3. First & FirstOrDefault
+            int resFirst = Algorithms.First(list, x => x % 2 == 0);
+            var resFirstDefault = Algorithms.FirstOrDefault(dict, x => x.Value > 100);
+
+            // 4. Single
+            int resSingle = Algorithms.Single(list, x => x == 5);
+
+            // 5. Any & All
+            bool resAny = Algorithms.Any(list, x => x < 0);
+            bool resAll = Algorithms.All(dict, x => x.Value > 1);
+
+            // 6. Count
+            int resCount1 = Algorithms.Count(list, x => x < 15);
+            int resCount2 = Algorithms.Count(dict, x => x.Value < 20);
+
+            // 7. Distinct
+            var resDistinct = Algorithms.Distinct(list);
+
+            // Print Results
+            Console.WriteLine("Where List: " + string.Join(", ", resWhereList));
+            Console.WriteLine("Where Dict Count: " + Algorithms.Count(resWhereDict, x => x.Value <= int.MaxValue));
+            Console.WriteLine("Order List: " + string.Join(", ", resOrderList));
+            Console.WriteLine("First Match: " + resFirst);
+            Console.WriteLine("FirstOrDef Missing Key: " + (resFirstDefault.Key ?? "null"));
+            Console.WriteLine("Single Match: " + resSingle);
+            Console.WriteLine("Any Negative: " + resAny);
+            Console.WriteLine("All Greater Than 1: " + resAll);
+            Console.WriteLine("Total Count: " + resCount1);
+            Console.WriteLine("Filtered Count: " + resCount2);
+            Console.WriteLine("Distinct List: " + string.Join(", ", resDistinct));
+
+            Console.ReadKey();
+        }
+    }
+}
